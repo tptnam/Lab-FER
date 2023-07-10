c8d8fa57dca90bc705e0e2f4c89b9245d67eb1e4
ThanhNam <trannam.nt865@gmail.com>
2023-07-10 16:13:33 +0700

    update lab 4
    done hehe

--- src/component/contact.js
+++ src/component/contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
-        <div id="contact">
-            <a href="https://www.facebook.com/tptnam"> FB của em nè thầy :v </a>
+        <div className="contact" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '81.5vh' }} >
+            <a href="https://www.facebook.com/tptnam"> FB của em nè thầy :v </a> <br></br>
             Nhanh gọn hehee
         </div>
     )
